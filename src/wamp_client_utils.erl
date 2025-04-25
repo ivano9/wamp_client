e86f338b0d7b44a8beb17450cfa63c7cf72e8be7
Alejandro Miguez <alejandro.miguez@leapsight.com>
2025-04-25 13:05:23 -0300

    Fix atom name generated used for process registrations

--- src/wamp_client_utils.erl
+++ src/wamp_client_utils.erl
@@ -3,7 +3,6 @@
 
 %% API
 -export([build_registration_name/1]).
--export([build_registration_name/2]).
 
 
 
@@ -21,17 +20,5 @@
 %% -----------------------------------------------------------------------------
 -spec build_registration_name([atom() | undefined]) -> atom().
 build_registration_name(Parts) ->
-    build_registration_name(Parts, "-").
-
-
-%% -----------------------------------------------------------------------------
-%% @doc It builds a registration name for the given list of atoms
-%% @param Parts The list of atoms to be joined
-%% @param Separator The separator to be used between the atoms
-%% @return The registration name as an atom
-%% @end
-%% -----------------------------------------------------------------------------
--spec build_registration_name([atom() | undefined], string()) -> atom().
-build_registration_name(Parts, Separator) ->
     StringParts = [atom_to_list(P) || P <- Parts, P =/= undefined],
-    list_to_atom(string:join(StringParts, Separator)).
+    list_to_atom(string:join(StringParts, "_")).
